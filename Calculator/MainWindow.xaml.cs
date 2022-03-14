7f6fa8c0434148cad9ca21f7d50afe0c5291d2af
Wojciech Królik <bylic22@gmail.com>
2022-03-14 18:59:07 +0100

    Basic aritmetic operations.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,71 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private double x = 0;
+        private double y = 0;
+        private string operation = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if(button.Content.ToString() == "+/-")
+            {
+                if(display.Text.Contains("-"))
+                {
+                    display.Text = display.Text.Replace("-","");
+                }
+                else
+                {
+                    display.Text = "-" + display.Text;
+                }
+                return;
+            }
+
+            if (!display.Text.Contains(".") && button.Content.ToString() == ",")
+            {
+                display.Text += ".";
+                return;
+            }
+            else if(button.Content.ToString()==",")
+            {
+                MessageBox.Show("Nie można dodać dwóch przecinków.");
+                return;
+            }
+
+            display.Text += button.Content.ToString();
+        }
+
+        private void Aritmetic_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (display.Text!="")
+            {
+                operation += "(" + display.Text + ")" + button.Content.ToString();
+            }
+            display.Text = "";
+        }
+
+        private void Result_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            if(display.Text != "")
+            {
+                operation += "(" + display.Text + ")";
+                var v = dt.Compute(operation,"");
+                display.Text = v.ToString();
+                if (display.Text.Contains(","))
+                {
+                    display.Text = display.Text.Replace(",", ".");
+                }
+            }
+        operation = "";
         }
     }
 }
